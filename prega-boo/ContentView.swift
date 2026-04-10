063cccda58b9379bbac8017b25d4fe40b1776987
viranga-lakshan <virangalakshanz@gmail.com>
2026-04-10 10:15:20 +0530

    Initial Commit

---
+++ prega-boo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prega-boo
+//
+//  Created by COBSCCOMP242P-068 on 2026-04-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prega-boo/prega_booApp.swift
@@ -0,0 +1,17 @@
+//
+//  prega_booApp.swift
+//  prega-boo
+//
+//  Created by COBSCCOMP242P-068 on 2026-04-10.
+//
+
+import SwiftUI
+
+@main
+struct prega_booApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
